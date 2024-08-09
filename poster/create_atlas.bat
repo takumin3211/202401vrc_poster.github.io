ff1c75162337427f9f70267f59d455e91d495daf
takumin3211 <enerc01@outlook.com>
2024-08-09 09:15:37 +0900

    2024/08/09

---
+++ poster/create_atlas.bat
@@ -0,0 +1,3 @@
+@echo off
+D:\hobby\software\ImageMagick-7.1.1-Q16\magick.exe montage -geometry 724x1024! D:\hobby\software\ImageMagick-7.1.1-Q16\1.png D:\hobby\software\ImageMagick-7.1.1-Q16\2.png D:\hobby\software\ImageMagick-7.1.1-Q16\3.png D:\hobby\software\ImageMagick-7.1.1-Q16\4.png -gravity center -tile 2x2 png32:D:\hobby\vrc\GitHub\202401vrc\poster\atlas_image.png
+pause
